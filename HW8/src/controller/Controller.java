bd499f5e34966dfe9b8e3c7e087ca99d09a1649f
Onsang1 <113162579+Onsang1@users.noreply.github.com>
2022-12-05 01:45:36 -0500

    Update Controller.java

--- HW8/src/controller/Controller.java
+++ HW8/src/controller/Controller.java
@@ -1,6 +1,7 @@
 
 /** @author Onsang Yau, Abd */
 
+import courses.Course;
 import files.FileInfoReader;
 
 public class Controller {
@@ -11,8 +12,11 @@ public class Controller {
         // randomly place 10 ships in ocean, 1 battleship, 2 cruiser, 3 destroyer, and 4 submarines
         reader.readFromCourseFile("courseInfo.txt");
         String[] result= reader.getCourseStr();
-        for (String a : result) {
-            System.out.println(a);
+//        for (String a : result) {
+//            System.out.println(a);
+//        }
+        for (Course c : reader.getCourseObj()) {
+            System.out.println(c.print());
         }
 
     }
