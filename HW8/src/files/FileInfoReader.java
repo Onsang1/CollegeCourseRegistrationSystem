e3ba0894ff3c1826d387af3fa147efdd32d95d1f
Onsang1 <113162579+Onsang1@users.noreply.github.com>
2022-12-04 20:51:56 -0500

    Update FileInfoReader.java

--- HW8/src/files/FileInfoReader.java
+++ HW8/src/files/FileInfoReader.java
@@ -1,5 +1,17 @@
 package files;
+import java.io.BufferedReader;
 
 public class FileInfoReader {
-
+    
+    /**
+     * Read all courses from the course Info file
+     * @param fileName = name of file
+     * @return an array containing all the courses
+     */
+    public String[] readFromCourseFile(String fileName) {
+        String[] allCourses = new String[0];
+        try {
+            int 
+        }
+    }
 }
