fea3754681b7a97866b14c02e328fec91bb1fee2
Onsang1 <113162579+Onsang1@users.noreply.github.com>
2022-12-05 21:25:43 -0500

    printout all working

--- HW8/src/controller/Controller.java
+++ HW8/src/controller/Controller.java
@@ -1,22 +1,42 @@
 
-/** @author Onsang Yau, Abd */
+/** @author Onsang Yau,  */
 
-import courses.Course;
 import files.FileInfoReader;
+import roles.Admin;
 
 public class Controller {
 
     public static void main(String[] args) {
 
+        // way of printing courses
         FileInfoReader reader= new FileInfoReader();
-
         reader.readFromCourseFile("courseInfo.txt");
-        String[] result= reader.getCourseStr();
-//        for (String a : result) {
-//            System.out.println(a);
+//        for (Course c : reader.getCourseObj()) {
+//            System.out.println(c.print());
 //        }
-        for (Course c : reader.getCourseObj()) {
-            System.out.println(c.print());
+
+        // way of printing students
+        FileInfoReader reader2= new FileInfoReader();
+        // randomly place 10 ships in ocean, 1 battleship, 2 cruiser, 3 destroyer, and 4 submarines
+        reader2.readFromStudentFile("studentInfo.txt");
+//        for (Student s : reader2.getStudentObj()) {
+//            System.out.println(s.print());
+//        }
+
+        // way of printing prof
+        FileInfoReader reader3= new FileInfoReader();
+        // randomly place 10 ships in ocean, 1 battleship, 2 cruiser, 3 destroyer, and 4 submarines
+        reader3.readFromProfFile("profInfo.txt");
+//        for (Professor p : reader3.getProfObj()) {
+//            System.out.println(p.print());
+//        }
+
+        // way of printing prof
+        FileInfoReader reader4= new FileInfoReader();
+        // randomly place 10 ships in ocean, 1 battleship, 2 cruiser, 3 destroyer, and 4 submarines
+        reader4.readFromAdminFile("adminInfo.txt");
+        for (Admin a : reader4.getAdminObj()) {
+            System.out.println(a.print());
         }
 
     }
--- HW8/src/files/FileInfoReader.java
+++ HW8/src/files/FileInfoReader.java
@@ -7,12 +7,66 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import courses.Course;
+import roles.Admin;
+import roles.Professor;
+import roles.Student;
+import roles.User;
 
 public class FileInfoReader {
     // initiate an ArrayList of Courses
     private ArrayList<Course> allCoursesObj= new ArrayList<>();
     // initiate an array of strings
-    private String[] allCoursesStr;
+//    private String[] allCoursesStr;
+    // an ArrayList of Admin
+    private ArrayList<Admin> allAdminObj= new ArrayList<>();
+    // an ArrayList of Professor
+    private ArrayList<Professor> allProfObj= new ArrayList<>();
+    // an ArrayList of Student
+    private ArrayList<Student> allStudentObj= new ArrayList<>();
+    // an ArrayList of User
+    private ArrayList<User> allUserObj= new ArrayList<>();
+
+    /** Read all Student from the ProfInfo file
+     *
+     * @param fileName = name of file
+     * @return an array of containing all the student obj */
+    public void readFromStudentFile(String fileName) {
+        try {
+            populateStudent(allStudentObj, fileName);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /** Read all Prof from the ProfInfo file
+     *
+     * @param fileName = name of file
+     * @return an array of containing all the admin obj */
+    public void readFromProfFile(String fileName) {
+        try {
+            populateProf(allProfObj, fileName);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /** Read all admins from the adminInfo file
+     *
+     * @param fileName = name of file
+     * @return an array of containing all the admin obj */
+    public void readFromAdminFile(String fileName) {
+        try {
+            populateAdmin(allAdminObj, fileName);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     /** Read all courses from the course Info file
      *
@@ -20,12 +74,12 @@ public class FileInfoReader {
      * @return an array containing all the courses */
     public void readFromCourseFile(String fileName) {
 
-        allCoursesStr= new String[0];
+//        allCoursesStr= new String[0];
         try {
-            int count= getCountOfCourses(fileName);
-            allCoursesStr= new String[count];
+//            int count= getCountOfLines(fileName);
+//            allCoursesStr= new String[count];
             // populate both the arraylist and the array of string for later use
-            populateCourses(allCoursesObj, allCoursesStr, fileName);
+            populateCourses(allCoursesObj, fileName);
             // exception handling
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -34,13 +88,18 @@ public class FileInfoReader {
         }
     }
 
-    public void populateCourses(ArrayList<Course> c, String[] CourseListString, String inputFile)
+    /** populate the Course arraylist with read files
+     *
+     * @param c:         is any ArrayList of Course obj
+     * @param inputFile: input file name
+     * @throws IOException: input output exception */
+    public void populateCourses(ArrayList<Course> c, String inputFile)
         throws IOException {
 
         // create buffer reader
         BufferedReader br= new BufferedReader(new FileReader(inputFile));
         // used to to populate String[] later
-        int index= 0;
+//        int index= 0;
         String line= "";
         while ((line= br.readLine()) != null) {
             // strip each line in case there are leading/ trailing white spaces
@@ -50,8 +109,81 @@ public class FileInfoReader {
                 // populate ArrayList c with a course obj made from each line of file
                 c.add(makeACourse(line));
                 // populate String[] with each line (strings)
-                CourseListString[index]= line;
-                index++ ;
+//                CourseListString[index]= line;
+//                index++ ;
+            }
+        }
+        br.close();
+    }
+
+    /** populate the Admin arraylist with read files
+     *
+     * @param c:         is any ArrayList of Admin obj
+     * @param inputFile: input file name
+     * @throws IOException: input output exception */
+    public void populateAdmin(ArrayList<Admin> c, String inputFile)
+        throws IOException {
+
+        // create buffer reader
+        BufferedReader br= new BufferedReader(new FileReader(inputFile));
+        // used to to populate String[] later
+//       int index= 0;
+        String line= "";
+        while ((line= br.readLine()) != null) {
+            // strip each line in case there are leading/ trailing white spaces
+            line= line.strip();
+            // if line is empty, skip it
+            if (!line.isEmpty()) {
+                // populate ArrayList c with an admin obj made from each line of file
+                c.add(makeAAdmin(line));
+            }
+        }
+        br.close();
+    }
+
+    /** populate the Prof arraylist with read files
+     *
+     * @param c:         is any ArrayList of Prof obj
+     * @param inputFile: input file name
+     * @throws IOException: input output exception */
+    public void populateProf(ArrayList<Professor> c, String inputFile)
+        throws IOException {
+
+        // create buffer reader
+        BufferedReader br= new BufferedReader(new FileReader(inputFile));
+        // used to to populate String[] later
+        String line= "";
+        while ((line= br.readLine()) != null) {
+            // strip each line in case there are leading/ trailing white spaces
+            line= line.strip();
+            // if line is empty, skip it
+            if (!line.isEmpty()) {
+                // populate ArrayList c with a prof obj made from each line of file
+                c.add(makeAProf(line));
+            }
+        }
+        br.close();
+    }
+
+    /** populate the Student arraylist with read files
+     *
+     * @param c:         is any ArrayList of Student obj
+     * @param inputFile: input file name
+     * @throws IOException: input output exception */
+    public void populateStudent(ArrayList<Student> c, String inputFile)
+        throws IOException {
+
+        // create buffer reader
+        BufferedReader br= new BufferedReader(new FileReader(inputFile));
+        // used to to populate String[] later
+        String line= "";
+        while ((line= br.readLine()) != null) {
+            // strip each line in case there are leading/ trailing white spaces
+            line= line.strip();
+            // if line is empty, skip it
+            if (!line.isEmpty()) {
+                // populate ArrayList c with a prof obj made from each line of file
+                c.add(makeAStudent(line));
             }
         }
         br.close();
@@ -79,12 +211,62 @@ public class FileInfoReader {
         return result;
     }
 
+    /** make an Admin obj
+     *
+     * @param line: a string that should be a line from the txt file
+     * @return an Admin obj */
+    public Admin makeAAdmin(String line) {
+        String info[]= line.split(";");
+        String ID= info[0].strip();
+        String name= info[1].strip();
+        String username= info[2].strip();
+        String password= info[3].strip();
+        // username, password, ID, name
+        Admin administrator= new Admin(username, password, ID, name);
+        return administrator;
+    }
+
+    /** make a Prof obj
+     *
+     * @param line: a string that should be a line from the txt file
+     * @return a Prof obj */
+    public Professor makeAProf(String line) {
+        String info[]= line.split(";");
+        String name= info[0].strip();
+        String ID= info[1].strip();
+        String username= info[2].strip();
+        String password= info[3].strip();
+        // username, password, ID, name
+        Professor prof= new Professor(username, password, ID, name);
+        return prof;
+    }
+
+    /** make a Student obj
+     *
+     * @param line: a string that should be a line from the txt file
+     * @return a Student obj */
+    public Student makeAStudent(String line) {
+        String info[]= line.split(";");
+        String ID= info[0].strip();
+        String name= info[1].strip();
+        String username= info[2].strip();
+        String password= info[3].strip();
+        String grades[]= info[4].strip().split(",");
+        ArrayList<String> gradeList= new ArrayList<>();
+        for (String i : grades) {
+            gradeList.add(i.strip());
+        }
+        // username, password, ID, name
+        Student student= new Student(username, password, ID, name, gradeList);
+        return student;
+    }
+
     /** Get the count of lines in the given file
      *
      * @param inputFile to read
      * @return count of (lines)
      * @throws IOException if I/O error occurs */
-    public int getCountOfCourses(String inputFile) throws IOException {
+    private int getCountOfLines(String inputFile) throws IOException {
 
         // create buffered reader
         BufferedReader br= new BufferedReader(new FileReader(inputFile));
@@ -112,10 +294,32 @@ public class FileInfoReader {
         return allCoursesObj;
     }
 
-    /** return the string array with all courses in string form
+    /** return the arraylist with all admins in obj form
      *
      * @return */
-    public String[] getCourseStr() {
-        return allCoursesStr;
+    public ArrayList<Admin> getAdminObj() {
+        return allAdminObj;
     }
+
+    /** return the arraylist with all Prof in obj form
+     *
+     * @return */
+    public ArrayList<Professor> getProfObj() {
+        return allProfObj;
+    }
+
+    /** return the arraylist with all Student in obj form
+     *
+     * @return */
+    public ArrayList<Student> getStudentObj() {
+        return allStudentObj;
+    }
+
+//    /** return the string array with all courses in string form
+//     *
+//     * @return */
+//    public String[] getCourseStr() {
+//        return allCoursesStr;
+//    }
+
 }
