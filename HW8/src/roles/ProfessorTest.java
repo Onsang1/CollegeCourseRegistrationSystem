0ea9d92a01db90ca6ed4c5f74552cd3666ea9d87
jingzhuochelseahu <106459071+jingzhuochelseahu@users.noreply.github.com>
2022-12-07 15:40:51 -0500

    Update ProfessorTest.java

--- HW8/src/roles/ProfessorTest.java
+++ HW8/src/roles/ProfessorTest.java
@@ -1,12 +1,62 @@
+package roles;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import roles.User;
+import courses.Course;
+import files.FileInfoReader;
+import courses.Course;
 
 class ProfessorTest {
 
-    @Test
-    void test() {
-        fail("Not yet implemented");
+	Professor professor;
+	Student student;
+	FileInfoReader reader= new FileInfoReader();
+	FileInfoReader reader2= new FileInfoReader();
+	ArrayList<Course> courseInfo;
+	ArrayList<Student> studentInfo;
+
+	@BeforeEach
+	void setUp() throws Exception {
+        //initialize professor
+		professor = new Professor("Clayton Greenberg", "001", "Greenberg", "password590");
+		reader.readFromCourseFile("courseInfo.txt");
+		reader2.readFromStudentFile("studentInfo.txt");
+		courseInfo = reader.getCourseObj();
+		studentInfo = reader2.getStudentObj();
     }
+
+	@Test
+	void testgetGivenCourses() {
+		//test if the two courses that professor Clayton Greenberg teach have been included
+		Course CIT592 = new Course("CIT592", "Mathematical Foundations of Computer Science", "Clayton Greenberg", "TR", "10:00", "11:00", 72);
+		assertEquals(CIT592.getCourseCode(), professor.getGivenCourses(courseInfo).get(0).getCourseCode());
+		
+		Course CIS105 = new Course("CIS105", "Computational Data Exploration", "Clayton Greenberg", "MWF", "10:00", "11:00", 60);
+		assertEquals(CIS105.getCourseCode(), professor.getGivenCourses(courseInfo).get(1).getCourseCode());
+		
+		//test if the course that professor Clayton Greenberg doesn't teach have been included
+		Course CIT902 = new Course("CIT901", "Course", "new professor", "TR", "10:30", "11:30", 72);
+		assertFalse(professor.getGivenCourses(courseInfo).contains(CIT902));
+	}
+	
+	@Test
+	void testgetStudentList() {
+		//test the student list for an empty course CIT800
+		Course CIT800 = new Course("CIT800", "Mathematical Foundations of Computer Science", "Clayton Greenberg", "TR", "10:00", "11:00", 72);
+		assertFalse(professor.getStudentList(CIT800, studentInfo).contains(studentInfo.get(0)));
+		
+		//test the student list for a non-empty course 
+		//ceate a new student
+		student = studentInfo.get(0);
+		//the student add the course
+		student.addCourse(courseInfo.get(0), courseInfo);
+		assertTrue(professor.getStudentList(courseInfo.get(0), studentInfo).contains(student));
+	}
 
 }
