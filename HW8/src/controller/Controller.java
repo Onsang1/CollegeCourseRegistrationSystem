97d76ea5a7b63cb80a4275cf44898a6c68cbba35
Onsang1 <113162579+Onsang1@users.noreply.github.com>
2022-12-07 14:01:38 -0500

    Update Controller.java

--- HW8/src/controller/Controller.java
+++ HW8/src/controller/Controller.java
@@ -1,45 +1,417 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import courses.Course;
+import files.FileInfoReader;
+import roles.Admin;
+import roles.Professor;
+import roles.Student;
 
 /** @author Onsang Yau, Abd */
-
-import files.FileInfoReader;
 
 public class Controller {
 
     public static void main(String[] args) {
-
-        // way of printing courses
+        Controller controller= new Controller();
         FileInfoReader reader= new FileInfoReader();
+        reader.readFromStudentFile("studentInfo.txt");
+        reader.readFromAdminFile("adminInfo.txt");
+        reader.readFromProfFile("profInfo.txt");
         reader.readFromCourseFile("courseInfo.txt");
-//        for (Course c : reader.getCourseObj()) {
-//            System.out.println(c.print());
-//        }
-        System.out.println(reader.getCourseObj().get(0).getStartTimeinNum());
-        System.out.println(reader.getCourseObj().get(0).getEndTimeinNum());
-        System.out.println(reader.getCourseObj().get(0).getDuration());
+        controller.run(reader);
+    }
 
-        // way of printing students
-        FileInfoReader reader2= new FileInfoReader();
-        reader2.readFromStudentFile("studentInfo.txt");
-//        for (Student s : reader2.getStudentObj()) {
-//            System.out.println(s.print());
-//        }
-        System.out.println(reader2.getStudentObj().get(1).getGrades());
-        // return a set view of keys contained in the map
-        System.out.println(reader2.getStudentObj().get(1).getGradesInMap().keySet());
+    public void run(FileInfoReader reader) {
 
-        // way of printing prof
-        FileInfoReader reader3= new FileInfoReader();
-        reader3.readFromProfFile("profInfo.txt");
-//        for (Professor p : reader3.getProfObj()) {
-//            System.out.println(p.print());
-//        }
+        System.out.println("--------------------------");
+        System.out.println("Students Management System");
+        System.out.println("--------------------------");
+        System.out.println("1 -- Login as a student");
+        System.out.println("2 -- Login as a professor");
+        System.out.println("3 -- Login as an admin");
+        System.out.println("4 -- Quit the system");
+        System.out.println("\n");
+        Scanner scanner= new Scanner(System.in);
+        boolean QuitTheSystem= false;
+        while (!QuitTheSystem) {
+            int LoginType= askForNum("Please enter your option, e.g. '1'", scanner, 1, 4);
+            // if input = 1, 2, or 3
+            if (LoginType != 4) {
+                // if user is a student
+                if (LoginType == 1) {
+                    if (!StudentLogin(scanner, reader)) { run(reader); }
+                } else if (LoginType == 2) {
+                    // if (!ProfessorLogin(scanner)) { run(); }
+                } else if (LoginType == 3) { if (!AdminLogin(scanner, reader)) { run(reader); } }
+            }
 
-        // way of printing prof
-        FileInfoReader reader4= new FileInfoReader();
-        reader4.readFromAdminFile("adminInfo.txt");
-//        for (Admin a : reader4.getAdminObj()) {
-//            System.out.println(a.print());
-//        }
+            if (LoginType == 4) {
+                QuitTheSystem= true;
+                System.out.println("Quitted Successfully.");
+            }
+        }
+        scanner.close();
+
+    }
+
+    private static boolean AdminLogin(Scanner scanner, FileInfoReader reader) {
+        Admin ourAdmin= null;
+        boolean loopcontinues= true;
+        do {
+            // Store username and password
+            String username= askForString("Please enter your username, or type 'q' to quit",
+                scanner);
+            if (username.equals("q")) { return false; }
+            // store password
+            String password= askForString("Please enter your password, or type 'q' to quit",
+                scanner);
+            if (password.equals("q")) { return false; }
+
+            // find our admin object
+            ourAdmin= checkAdminLogin(reader.getAdminObj(), username, password);
+
+            if (ourAdmin != null) {
+                printMenus(2, username);
+                int answer= askForNum("Please enter your option, e.g. '1'.", scanner, 1, 8);
+                if (answer != 8) {
+                    // view all courses
+                    if (answer == 1) {
+                        for (Course c : reader.getCourseObj()) {
+                            System.out.println(c.print());
+                        }
+                    } else if (answer == 2) {
+                        // check if the course exists
+                        String CourseID= AdminAddCourse(reader,
+                            "Please enter the course ID, or type 'q' to quit. e.g. 'CIT800'",
+                            scanner);
+                        if (CourseID.equals("q")) { return false; }
+
+                        String name= askForString(
+                            "Please enter the course name, or type 'q' to quit. e.g. 'Java' ",
+                            scanner);
+                        if (name.equals("q")) { return false; }
+                        String startTime= askForString(
+                            "Please enter the course start time, or type 'q' to quit. e.g. '20:00'",
+                            scanner);
+                        if (startTime.equals("q")) { return false; }
+                        String endTime= askForString(
+                            "Please enter the course end time, or type 'q' to quit. e.g. '21:00'",
+                            scanner);
+                        if (endTime.equals("q")) { return false; }
+                        String date= askForString(
+                            "Please enter the course date, or type 'q' to quit. e.g. 'MW'",
+                            scanner);
+                        if (endTime.equals("q")) { return false; }
+                        String capacity= askForString(
+                            "Please enter the course capacity, or type 'q' to quit. e.g. '72'",
+                            scanner);
+                        if (capacity.equals("q")) { return false; }
+                        // if capacity != 'q', we turn it into an integer
+                        Integer.parseInt(capacity);
+                        String lecturerId= askForString(
+                            "Please enter the course lecturer's id, or type 'q' to quit. e.g. '001'",
+                            scanner);
+                        if (lecturerId.equals("q")) { return false; }
+                        // check if professor exists
+                        boolean ProfExists= professorExists(lecturerId, reader.getProfObj());
+                        if (ProfExists) {}
+                        // if ProfExists = false, create a new prof
+                        else {
+
+                        }
+//                        ourAdmin.addNewCourse(null, null, professorExist(),
+//                            null);
+
+                    }
+
+                    // if answer == 8
+                } else {
+                    loopcontinues= false;
+
+                }
+
+                // if ourAdmin == null
+            } else {
+                System.out
+                    .println("Your username and password combination is wrong. Please try again!");
+            }
+
+        } while (loopcontinues);
+        return false;
+
+    }
+
+    private static boolean StudentLogin(Scanner scanner, FileInfoReader reader) {
+        // store username
+        String username= askForString("Please enter your username, or type 'q' to quit", scanner);
+        if (username.equals("q")) { return false; }
+        // store password
+        String password= askForString("Please enter your password, or type 'q' to quit", scanner);
+        if (password.equals("q")) { return false; }
+
+        // find our Student object
+        Student ourStudent= checkStudentLogin(reader.getStudentObj(), username, password);
+        if (ourStudent != null) {
+            boolean loopContinues= true;
+            while (loopContinues) {
+                printMenus(1, username);
+                int answer= askForNum("Please enter your option, e.g. '1'.", scanner, 1, 6);
+//                System.out.println("Line 72:" + answer);
+                if (answer != 6) {
+                    // 1 -- View all courses
+                    if (answer == 1) {
+//                        System.out.println("Testing testing");
+                        for (Course c : reader.getCourseObj()) {
+                            System.out.println(c.print());
+                        }
+                        // 2 -- Add courses to your list
+                    } else if (answer == 2) {
+                        Course course= StudentaddCourses(scanner, reader);
+                        System.out.println("The courses in your list are: ");// TODO
+
+                        // 3 -- View enrolled courses
+                    } else if (answer == 3) {
+//                        System.out.println(ourStudent.g) // TODO
+
+                        // 4 -- Drop courses in your list
+                    } else if (answer == 4) {
+                        // ourStudent.dropxxx
+
+                        // 5 -- View grades
+                    } else if (answer == 5) {
+
+                    }
+
+                    // if answer == 6: quit to main menu
+                } else {
+                    loopContinues= false;
+                }
+            }
+            // if the student entered wrong combination of username and passwords
+        } else {
+            System.out
+                .println("Your username and password combination is wrong. Please try again!");
+            return StudentLogin(scanner, reader);
+        }
+//        System.out.println("Line 104 reached");
+        return false;
+    }
+
+    /** This function prints the menus based on user type
+     *
+     * @param: type     is an integer; 1 = student, 2= admin, 3 = prof
+     * @param: username as String */
+    private static void printMenus(int type, String username) {
+        System.out.println("--------------------------");
+        System.out.println("Welcome, " + username);
+        System.out.println("--------------------------");
+        // student
+        if (type == 1) {
+            System.out.println("1 -- View all courses");
+            System.out.println("2 -- Add courses to your list");
+            System.out.println("3 -- View enrolled courses");
+            System.out.println("4 -- Drop courses in your list");
+            System.out.println("5 -- View grades");
+            System.out.println("6 -- Return to previous menu");
+        }
+        // admin
+        else if (type == 2) {
+
+            System.out.println("1 -- View all courses");
+            System.out.println("2 -- Add new courses");
+            System.out.println("3 -- Delete courses");
+            System.out.println("4 -- Add new professor");
+            System.out.println("5 -- Delete professor");
+            System.out.println("6 -- Add new student");
+            System.out.println("7 -- Delete a student");
+            System.out.println("8 -- return to previous menu");
+
+        }
+    }
+
+    /** This function check if the admin with the username and password exist
+     *
+     * @param list
+     * @param username
+     * @return */
+    private static Admin checkAdminLogin(ArrayList<Admin> list, String username,
+        String password) {
+        for (Admin a : list) {
+            if (a.getUserName().equals(username) && a.getPassword().equals(password)) { return a; }
+        }
+        return null;
+    }
+
+    /** This function check if the student with the username exist
+     *
+     * @param list
+     * @param username
+     * @return */
+    private static Student checkStudentLogin(ArrayList<Student> list, String username,
+        String password) {
+        for (Student s : list) {
+            if (s.getUserName().equals(username) && s.getPassword().equals(password)) { return s; }
+        }
+        return null;
+    }
+
+    /** check if the professor is already in the profInfo
+     *
+     * @param professorID
+     * @param profInfo
+     * @return */
+    private static boolean professorExists(String Id, ArrayList<Professor> profInfo) {
+        for (Professor p : profInfo) {
+            if (p.getID().equals(Id)) { return true; }
+        }
+        return false;
+    }
+
+    /** check if an entered coursecode is in the current course list
+     *
+     * @param course:     an arraylist of Course obj
+     * @param courseCode: a string
+     * @return */
+    private static Course checkCourseToAdd(ArrayList<Course> course, String courseCode) {
+        for (Course c : course) {
+            if (c.getCourseCode().equals(courseCode)) { return c; }
+        }
+        return null;
+    }
+
+    /** do the action of adding a course that user selected to the user's cart
+     *
+     * @param scanner: a scanner
+     * @param reader:  the fileinforeader */
+    private static Course StudentaddCourses(Scanner scanner, FileInfoReader reader) {
+        boolean keeptheloop2= true;
+        System.out.println("Which course do you want to add to your cart?");
+
+        do {
+            String addCourse= askForString("Please enter the course number that you want to add: ",
+                scanner);
+            Course courseToAdd= checkCourseToAdd(reader.getCourseObj(), addCourse);
+            if (courseToAdd == null) {
+                System.out
+                    .println(
+                        "The course code you entered is wrong/ not a valid course. Please try again!");
+            } else {
+                keeptheloop2= false;
+                // TODO: student.addCourse();
+                System.out.println(
+                    "You have successfully added " + courseToAdd.getCourseCode() +
+                        " in your cart!");
+                return courseToAdd;
+
+            }
+
+        } while (keeptheloop2);
+        return null;
+    }
+
+    private static String AdminAddCourse(FileInfoReader reader, String prompt, Scanner scanner) {
+        boolean loop= true;
+        String answer;
+        do {
+            answer= askForString(prompt, scanner);
+            if (checkCourseToAdd(reader.getCourseObj(), answer) != null) {
+                loop= false;
+            } else {
+                System.out.println(
+                    "Sorry, the course with the ID already exists in our database. Please enter a new course only.");
+            }
+
+        } while (loop);
+        return answer;
+    }
+
+    /** Asks the user for a number to log in to the system . Prompts again if the user input is not
+     * a valid integer, or if the number is not between the start and end number, inclusive.
+     *
+     * @param scanner for user input
+     * @param start   = the start number
+     * @param end     = the end number
+     * @return an integer indicating the type of user the user wants to log in with */
+    private static int askForNum(String prompt, Scanner scanner, int start, int end) {
+
+        // TODO Add your code here
+        // create an array that is from 0 to 9
+        ArrayList<Integer> array1= new ArrayList<>();
+        for (int i= start; i < end + 1; i++ ) { array1.add(i); }
+
+        int answer= -1;
+        boolean keeptheloop2= true;
+        // if the user answers something other than an integer/ the interger
+        // is not between 0 to 9, inclusive, prompt the method again.
+        System.out.println(prompt);
+        do {
+            System.out
+                .println("Enter an integer between " + start + " - " + end + ", inclusively:");
+            // if user entered an integer
+            if (scanner.hasNextInt()) {
+                // store that input
+                int tempAnswer= scanner.nextInt();
+                // if that integer is actually between the number
+                if (!array1.contains(tempAnswer)) {
+                    System.out.println("Your input is either out-of-bound or it is negative");
+                } else {
+                    answer= tempAnswer;
+                    keeptheloop2= false;
+                }
+            } else {
+                // catch for non-integer input
+                System.out.println("Your input is not an integer.");
+                scanner.next();
+            }
+        } while (keeptheloop2);
+        return answer;
+
+    }
+
+    /** Asks the user for a String. Prompts again if the user input is not a valid String
+     *
+     * @param scanner for user input
+     * @return a String answer */
+    private static String askForString(String prompt, Scanner scanner) {
+//        String answer= "";
+        System.out.println(prompt);
+        String answer= scanner.next().strip();
+        return answer;
 
     }
 }
+
+//// way of printing courses
+//FileInfoReader reader= new FileInfoReader();
+//reader.readFromCourseFile("courseInfo.txt");
+////for (Course c : reader.getCourseObj()) {
+////    System.out.println(c.print());
+////}
+//System.out.println(reader.getCourseObj().get(0).getStartTimeinNum());
+//System.out.println(reader.getCourseObj().get(0).getEndTimeinNum());
+//System.out.println(reader.getCourseObj().get(0).getDuration());
+//
+//// way of printing students
+//FileInfoReader reader2= new FileInfoReader();
+//reader2.readFromStudentFile("studentInfo.txt");
+////for (Student s : reader2.getStudentObj()) {
+////    System.out.println(s.print());
+////}
+//System.out.println(reader2.getStudentObj().get(1).getGrades());
+//// return a set view of keys contained in the map
+//System.out.println(reader2.getStudentObj().get(1).getGradesInMap().keySet());
+//System.out.println(reader2.getStudentObj().get(1).getStudentCourse());
+//
+//// way of printing prof
+//FileInfoReader reader3= new FileInfoReader();
+//reader3.readFromProfFile("profInfo.txt");
+////for (Professor p : reader3.getProfObj()) {
+////    System.out.println(p.print());
+////}
+//
+//// way of printing prof
+//FileInfoReader reader4= new FileInfoReader();
+//reader4.readFromAdminFile("adminInfo.txt");
+////for (Admin a : reader4.getAdminObj()) {
+////    System.out.println(a.print());
+////}
